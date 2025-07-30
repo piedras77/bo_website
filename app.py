92e9bfd13fa9e0cf802804b8756ae562d9135ab8
piedras77 <piedrasebastian@gmail.com>
2025-07-30 11:50:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect
 
 import sys
 import os
